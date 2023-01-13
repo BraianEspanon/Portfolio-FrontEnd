e628bfd08ac9e294ad842c163561285adba90ef2
BraianEspanon <107149922+BraianEspanon@users.noreply.github.com>
2023-01-13 14:29:09 -0300

    Redes sociales agregadas

--- src/app/components/header/header-bar/header-bar.component.ts
+++ src/app/components/header/header-bar/header-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header-bar',
@@ -7,7 +8,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./header-bar.component.css']
 })
 export class HeaderBarComponent implements OnInit {
-  argentinaProgramaLogo: string = "/assets/images/ArgentinaPrograma_logo.png"
+  argentinaProgramaLogo: string = "/assets/images/ArgentinaPrograma_logo.png";
+  faInstagram = faInstagram;
+  faTwitter = faTwitter
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
